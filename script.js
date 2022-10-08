b9af2790ca621b448bfe72e7e440b477a6c0f9bd
Pham Vu Thai Minh <thaiminhpv@gmail.com>
2022-10-08 16:00:06 +0700

    update new students

--- script.js
+++ script.js
@@ -46,51 +46,47 @@ function moveSeat() {
 var isHacked = false;
 var isButtonPressed = false;
 var map = [
-    'Diệu Anh',
-    'Hoàng Anh',
-    'Tuấn Bò',
-    'Quốc Anh',
-    'Quỳnh Anh',
-    'Vi Anh',
-    'N.Minh Châu',
-    'T.Minh Châu',
-    'Mai Chi',
-    'Minh Đức',
-    'Hương Giang',
-    'Ngân Hà',
-    'Hoàng Hạnh',
-    'Thái Hoàng',
-    'Xuân Hoàng',
-    'Quang Hợp',
-    'Văn Hùng',
-    'Quang Huy',
-    'Thu Hương',
-    'Nguyên Khôi',
-    'Trần Kiên',
-    'Ái Linh',
-    'Khánh Linh',
-    'Mai Ly',
-    'Hải Minh',
-    'Thu Minh',
-    'Hà My',
-    'Trà My',
-    'Minh Ngọc',
-    'Phương Ngọc',
-    'L.Hà Phương',
-    'H Hà Phương',
-    'Trần Quang',
-    'Diễm Quỳnh',
-    'Thuý Quỳnh',
-    'Phương Thảo',
-    'Thịnh Trần',
-    'Minh Thư',
-    'Anh Tú',
-    'Mạnh Tuấn',
-    'Quốc Tuấn',
-    'Hồ Uyên',
-    'Hoàng Việt',
-    'Hà Vy',
-    'Hường Vy'
+    'Lê Đào Mỹ Anh',
+    'Nguyễn Ngọc Anh',
+    'Phạm Đức Anh',
+    'Phan Hàn Anh',
+    'Nguyễn Gia Bảo',
+    'Lưu Bảo Châu',
+    'Nguyễn Minh Châu',
+    'Phạm Bảo Châu',
+    'Lê Hoàng Vân Chi',
+    'Phạm Tiến Dũng',
+    'Nguyễn Khánh Duy',
+    'Nguyễn Nhật Duy',
+    'Phạm Hải Đăng',
+    'Nguyễn Bảo Hân',
+    'Trần Nguyễn Gia Hân',
+    'Mai Khắc Hiếu',
+    'Nguyễn Khánh Huyền',
+    'Nguyễn Ngọc Khánh',
+    'Lê Phạm Bảo Lam',
+    'Đặng Phương Linh',
+    'Nguyễn Ngô Ngọc Linh',
+    'Nguyễn Phương Linh',
+    'Nguyễn Phương Linh',
+    'Bùi Thanh Mai',
+    'Hoàng Bình Minh',
+    'Lê Đức Minh',
+    'Đỗ Minh Nghĩa',
+    'Nguyễn Yến Ngọc',
+    'Nguyễn Vũ Tuệ Nhi',
+    'Phạm Duy Phước',
+    'Dương Xuân Quý',
+    'Bùi Thanh Thúy',
+    'Nguyễn Ngọc Anh Thư',
+    'Phạm Quốc Minh Quân',
+    'Phạm Anh Thư',
+    'Lê Thùy Trang',
+    'Bùi Bảo Trung',
+    'Phan Doãn Thành Trung',
+    'Vũ Minh Tú',
+    'Võ Thùy Vân',
+    'Nguyễn Hải Yến',
 ];
 
 
@@ -100,80 +96,9 @@ shuffle(map);
 
 
 $(document).ready(function () {
-    $("#teacher-table").on("click", function () {
-        isHacked = true;
-        if (isButtonPressed) {
-            return;
-        }
-        console.log("evil mode enabled");
-
-        //remove from list
-        var hacklist = [
-            'Thịnh Trần', 'Khánh Linh',
-            'Hoàng Việt', 'Quốc Tuấn',
-            'Hương Giang', 'Quỳnh Anh',
-            'Vi Anh', 'Hồ Uyên',
-            'Anh Tú', 'Thái Hoàng',
-            'Trà My', 'T.Minh Châu'
-        ];
-        for (var a in hacklist) {
-            console.log("removed " + map.splice(map.indexOf(hacklist[a]), 1) + " from list");
-        }
-        isButtonPressed = true;
-    });
-
     $(".desk button").on("click", function () {
         if (!isHacked) {
             this.innerHTML = map.pop();
-            this.disabled = true;
-            return;
-        } else {
-            var thisTable = $(this).parent();
-            if (thisTable.hasClass("thinh_khanhlinh")) {
-                console.log("thinh_khanhlinh table clicked!");
-                if (this == thisTable.children(":first")[0]) {
-                    this.innerHTML = 'Thịnh Trần';
-                } else {
-                    this.innerHTML = 'Khánh Linh';
-                }
-            } else if (thisTable.hasClass("hoangviet_quoctuan")) {
-                console.log("hoangviet_quoctuan table clicked!");
-                if (this == thisTable.children(":first")[0]) {
-                    this.innerHTML = 'Hoàng Việt';
-                } else {
-                    this.innerHTML = 'Quốc Tuấn';
-                }
-            } else if (thisTable.hasClass("huonggiang_quynhanh")) {
-                console.log("huonggiang_quynhanh table clicked!");
-                if (this == thisTable.children(":first")[0]) {
-                    this.innerHTML = 'Hương Giang';
-                } else {
-                    this.innerHTML = 'Quỳnh Anh';
-                }
-            } else if (thisTable.hasClass("vianh_huongvy")) {
-                console.log("vianh_huongvy table clicked!");
-                if (this == thisTable.children(":first")[0]) {
-                    this.innerHTML = 'Vi Anh';
-                } else {
-                    this.innerHTML = 'Hồ Uyên';
-                }
-            } else if (thisTable.hasClass("anhtu_thaihoang")) {
-                console.log("anhtu_thaihoang table clicked!");
-                if (this == thisTable.children(":first")[0]) {
-                    this.innerHTML = 'Anh Tú';
-                } else {
-                    this.innerHTML = 'Thái Hoàng';
-                }
-            } else if (thisTable.hasClass("tramy_minhchau")) {
-                console.log("tramy_minhchau table clicked!");
-                if (this == thisTable.children(":first")[0]) {
-                    this.innerHTML = 'Trà My';
-                } else {
-                    this.innerHTML = 'T.Minh Châu';
-                }
-            } else {
-                this.innerHTML = map.pop();
-            }
             this.disabled = true;
             return;
         }
